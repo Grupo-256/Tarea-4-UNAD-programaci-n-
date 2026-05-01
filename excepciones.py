f4d005888001d64a10b73d7692b161d7c34192eb
SamtyJimenez19 <santiagijimenez813@gmail.com>
2026-05-02 00:57:23 +0200

    Update excepciones.py

--- excepciones.py
+++ excepciones.py
@@ -1,1 +1,20 @@
+class SoftwareFJError(Exception):
+    """
+    Clase base.Sirve para agrupar todos nuestros errores bajo un mismo nombre. 
+    Es útil si queremos atrapar 'cualquier error que nosotros hayamos inventado'.
+    """
+    pass
 
+class DatoInvalidoError(SoftwareFJError):
+    """
+    Utilidad: Se lanza cuando el usuario escribe números negativos o 
+    deja campos vacíos. Evita que los cálculos matemáticos den resultados absurdos.
+    """
+    pass
+
+class ServicioNoDisponibleError(SoftwareFJError):
+    """
+    Utilidad: Útil para cuando un cliente intenta reservar una sala o equipo 
+    que ya está ocupado o no existe en el catálogo.
+    """
+    pass
